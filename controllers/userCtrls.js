bcb92ffea2a57b31250233e7c72b8a03382c37f5
Steve JobsEleanor <eleanorabrow@gmail.com>
2023-05-21 12:24:41 -0400

    creating routes

--- controllers/userCtrls.js
+++ controllers/userCtrls.js
@@ -1,15 +1,34 @@
-// const db = require('../models') //this is where our db mongoose connection lives as well as our models
+const db = require('../models') //this is where our db mongoose connection lives as well as our models
 
+// USER INDEX ROUTE
 const getUser = (req, res) => {
-  // db.People.find({})  <-- db has all our models in it so we can use any of them here with one line! 
-  res.send('getUser')
+    // db.User.find({})  <-- db has all our models in it so we can use any of them here with one line! 
+    // res.send('getUser')
+    db.User.find({})
+    .then((foundUser) => {
+        if(!foundUser){
+            res.status(404).json({message: 'Cannot find User'})
+        } else {
+            res.status(200).json({data: foundUser})
+        }
+    })
 }
 
+// USER CREATE ROUTE
 const createUser = (req, res) => {
-  // db.People.create({name: 'testing'})
-  // .then((res) => {console.log(res)})
-  res.send('createUser')
+    // db.User.create({name: 'testing'})
+    // .then((res) => {console.log(res)})
+    // res.send('createUser')
+    db.User.create(req.body)
+    .then((createdUser) => {
+        if(!createdUser){
+            res.status(400).json({message: 'Cannot create user'})
+        } else {
+            res.status(201).json({data: createdUser, message: 'User created'})
+        }
+    })
 }
+
 
 module.exports = {
   getUser, 
